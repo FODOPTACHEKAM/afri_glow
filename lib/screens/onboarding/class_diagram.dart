ea7d573d6d339631330e8999f8201d3f51721635
tracy-hub-star <nzelletracy@gmail.com>
2026-06-07 18:15:46 +0100

    class Diagram

---
+++ lib/screens/onboarding/class_diagram.dart
@@ -0,0 +1,32 @@
+class User {
+  int id;
+  String name;
+  String email;
+  String password;
+  String skinType;
+}
+
+class Product {
+  int id;
+  String name;
+  String description;
+  String benefits;
+}
+
+class SkinAnalysis {
+  int id;
+  String skinType;
+  double score;
+}
+
+class BeautyTip {
+  int id;
+  String title;
+  String content;
+}
+
+class RoutinePlanner {
+  int id;
+  String routineName;
+  String schedule;
+}
