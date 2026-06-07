1f2ea34bc403050c90e502891071aa6435962ba9
tracy-hub-star <nzelletracy@gmail.com>
2026-06-07 18:23:55 +0100

    changing latest

--- lib/screens/dashboard/dashboard_screen.dart
+++ lib/screens/dashboard/dashboard_screen.dart
@@ -12,8 +12,8 @@ class DashboardScreen extends StatelessWidget {
 
   Widget build(BuildContext context) {
     final provider = context.watch<AppProvider>();
+    final scores = provider.skinScoreHistory;
 
-    final latestScore = scores.isNotEmpty ? scores.last : 75.0;
     final routineSteps = 5;
     final doneSeps = provider.completedStepsToday;
     final routineProgress = (doneSeps / routineSteps).clamp(0.0, 1.0);
